1ada805cadd82b3f41756963a1ccc9038405d76e
gokugoku123 <49818345+gokugoku123@users.noreply.github.com>
2021-03-10 12:13:23 +0530

    Changed TestCase Names

--- spring/Bookstore/src/test/java/com/example/BookstoreApplicationTests.java
+++ spring/Bookstore/src/test/java/com/example/BookstoreApplicationTests.java
@@ -33,7 +33,7 @@ public class BookstoreApplicationTests {
 	 
 	//Testing Login and Sign Up 
 	@Test
-    public void TestCase1() throws Exception {
+    public void test_case1() throws Exception {
 		 
 		//Sign Up
 	 	String userOne = "{\"email\":\"Test@gmail.com\",\"username\":\"TestSignup\",\"mobileNumber\":\"1234598760\",\"role\":\"ADMIN\",\"password\":\"Testing\",\"confirmPassword\":\"Testing\"}";
@@ -60,7 +60,7 @@ public class BookstoreApplicationTests {
 	 
 	//Add Product
 	@Test
-	public void TestCase2() throws Exception {
+	public void test_case2() throws Exception {
 		String prodOne = "{\"productId\":\"123\",\"imageUrl\":\"TestingURL\",\"productName\":\"TestingName\",\"price\":\"100\",\"description\":\"TestingDescription\",\"quantity\":10}";
 	    mockMvc.perform(MockMvcRequestBuilders.post("/admin/addProduct")
 	    		.contentType(MediaType.APPLICATION_JSON)
@@ -73,7 +73,7 @@ public class BookstoreApplicationTests {
 	 
 	 //Home
 	 @Test
-	 public void TestCase3() throws Exception {
+	 public void test_case3() throws Exception {
 		 
 		 String prodOne = "{\"productId\":\"123\",\"imageUrl\":\"TestingURL1\",\"productName\":\"TestingName1\",\"price\":\"100\",\"description\":\"TestingDescription1\",\"quantity\":10}";
 		 mockMvc.perform(MockMvcRequestBuilders.post("/admin/addProduct")
@@ -93,7 +93,7 @@ public class BookstoreApplicationTests {
 	 
 	 
 	 @Test
-	 public void TestCase4() throws Exception {
+	 public void test_case4() throws Exception {
 		 
 		 String prodOne = "{\"productId\":\"123\",\"imageUrl\":\"TestingURL1\",\"productName\":\"TestingName1\",\"price\":\"100\",\"description\":\"TestingDescription1\",\"quantity\":10}";
 		 mockMvc.perform(MockMvcRequestBuilders.post("/admin/addProduct")
@@ -113,7 +113,7 @@ public class BookstoreApplicationTests {
 	 
 	 
 	 @Test
-	 public void TestCase5() throws Exception {
+	 public void test_case5() throws Exception {
 		 
 		 mockMvc.perform(MockMvcRequestBuilders.get("/user/Test@gmail.com/orders")
 				.accept(MediaType.APPLICATION_JSON))
